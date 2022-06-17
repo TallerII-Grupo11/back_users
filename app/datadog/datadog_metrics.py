b8bc31cb1b59e23258b3ecb62f9201d016e60f63
Elianagam <elianagam2@gmail.com>
2022-06-17 17:12:05 -0300

    Add other user case for metrics in datadog

--- app/datadog/datadog_metrics.py
+++ app/datadog/datadog_metrics.py
@@ -8,17 +8,46 @@ statsd = ThreadStats()
 
 ENVIRON = f"spotifiuby:{os.environ.get('ENVIRONMENT', 'test')}"
 
+"""
+    CA 1: Métricas de nuevos usuarios utilizando mail y contraseña
+    CA 2: Métricas de nuevos usuarios utilizando identidad federada
+    CA 3: Métricas de login de usuarios utilizando mail y contraseña
+    CA 4: Métricas de login de usuarios utilizando identidad federada
+    CA 5: Métricas de usuarios bloqueados
+    CA 6: Métricas de recupero de contraseña
+"""
 
-def update_users(count_users):
+
+def new_login():
+    """Métricas de login de usuarios utilizando mail y contraseña"""
+    statsd.increment(f'spotifiuby.new-login', tags=[ENVIRON])
+
+
+def new_user():
+    """Métricas de nuevos usuarios utilizando mail y contraseña"""
+    statsd.increment(f'spotifiuby.new-user', tags=[ENVIRON])
+    update_users()
+
+def new_login_federated():
+    """Métricas de login de usuarios utilizando identidad federada"""
+    metric = f'spotifiuby.new-login-federated'
+    statsd.increment(metric, tags=[ENVIRON])
+
+
+def new_user_federated():
+    """Métricas de nuevos usuarios utilizando identidad federada"""
+    metric = f'spotifiuby.new-user-federated'
+    statsd.increment(metric, tags=[ENVIRON])
+    update_users()
+
+def update_blocked_users():
+    """Métricas de usuarios bloqueados"""
     statsd.gauge(
-        'spotifiuby.total-users', count_users, tags=[ENVIRON],
+        'spotifiuby.blocked-users',
+        10, # obtener los usuarios bloqueados por una query a la bdd
+        tags=[ENVIRON],
     )
 
-
-def new_login(provider):
-    statsd.increment(f'spotifiuby.new-login.{provider}', tags=[ENVIRON])
-
-
-def new_user(provider):
-    statsd.increment(f'spotifiuby.new-user.{provider}', tags=[ENVIRON])
-    update_users()
+def password_reset():
+    """Métricas de recupero de contraseña"""
+    statsd.increment('spotifiuby.password-reset', tags=[ENVIRON])
