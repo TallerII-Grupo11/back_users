d39c17716b23e9b455512dbd9668419b87dc155b
Tomas Macia <tomasmacia@gmail.com>
2022-06-30 19:02:19 -0300

    Metrics usage

--- app/adapters/http/events/events_controller.py
+++ app/adapters/http/events/events_controller.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter, status
+import logging
+
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import Response
 
 from app.adapters.http.events.input.events import LoginRequest
+from app.adapters.rest.queue_metrics_client import QueueMetricsClient
 from app.datadog.datadog_metrics import DataDogMetric
+from app.dependencies.dependencies import get_restclient_metrics
 
 router = APIRouter(tags=["events"])
+logger = logging.getLogger(__name__)
 
 
 @router.post(
@@ -13,11 +18,17 @@ router = APIRouter(tags=["events"])
 )
 async def notify_login(
     login_request: LoginRequest,
+    rest_metrics: QueueMetricsClient = Depends(get_restclient_metrics),
 ):
     if login_request.federated:
         DataDogMetric.new_login_federated()
     else:
         DataDogMetric.new_login()
+
+    try:
+        rest_metrics.record_login(login_request.federated)
+    except Exception as e:
+        logger.error("Error in Queue Metrics request: ", e)
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -26,6 +37,14 @@ async def notify_login(
     '/password_reset',
     status_code=status.HTTP_204_NO_CONTENT,
 )
-async def notify_password_reset():
+async def notify_password_reset(
+    rest_metrics: QueueMetricsClient = Depends(get_restclient_metrics),
+):
     DataDogMetric.password_reset()
+
+    try:
+        rest_metrics.record_password_reset()
+    except Exception as e:
+        logger.error("Error in Queue Metrics request: ", e)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
